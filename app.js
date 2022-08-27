8256d8ef10e1bb2257e4f428691b9dd531b81549
Kim Hoang <kimberley.hoang528@gmail.com>
2022-08-27 07:41:12 -0400

    Fixed footer

--- app.js
+++ app.js
@@ -17,6 +17,22 @@ app.get("/topics", function(req,res){
   res.render("topics");
 });
 
+app.get("/game", function(req,res){
+  res.render("game");
+});
+
+app.post("/game", function(req,res){
+
+  // if(userAns === correctAns){
+  //   res.redirect("/success")
+  // } else {
+  //   res.redirect("/game");
+  //  code to make another attempt
+  // }
+
+});
+
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
