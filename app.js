baa558ee10de31ecfa2ff7d5364e2ece1a59487f
Kim Hoang <kimberley.hoang528@gmail.com>
2022-08-27 15:32:36 -0400

    Added stats page

--- app.js
+++ app.js
@@ -10,28 +10,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.render("game");
-});
-
-app.get("/topics", function(req,res){
   res.render("topics");
 });
 
-app.get("/game", function(req,res){
-  res.render("game");
+
+app.get("/stats", function(req,res){
+  res.render("stats");
 });
-
-app.post("/game", function(req,res){
-
-  // if(userAns === correctAns){
-  //   res.redirect("/success")
-  // } else {
-  //   res.redirect("/game");
-  //  code to make another attempt
-  // }
-
-});
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
